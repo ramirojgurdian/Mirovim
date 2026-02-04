a5dc8b628a7b3e90e76ce08fc7dd88212cc435b6
ramirojgurdian <ramirojgurdian@outlook.com>
2026-02-03 21:06:47 -0600

    Added more plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,8 @@
 require('lazy').setup({
+
+  --If you are not zyo you may want to delete this one
+  require('plugins.smear-cursor'),
+  -----------------------------------------------------
   require('plugins.neo-tree'),
   require('plugins.mason'),
   require('plugins.which-key'),
@@ -7,5 +11,8 @@ require('lazy').setup({
   require('plugins.lspconfig'),
   require('plugins.colorscheme'),
   require('plugins.treesitter'),
-  require('plugins.conform')
+  require('plugins.conform'),
+  require('plugins.autopairs'),
+  require('plugins.gitsigns'),
+  require('plugins.lualine'),
 })
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,24 +10,24 @@ return {
     'MunifTanjim/nui.nvim',
   },
   lazy = false,
-keys = {
-  {
-    "<C-e>",
-    function()
-      local ft = vim.bo.filetype
+  keys = {
+    {
+      "<C-e>",
+      function()
+        local ft = vim.bo.filetype
 
-      if ft == "neo-tree" then
-        -- Cursor is inside Neo-tree → close it
-        vim.cmd("Neotree close")
-      else
-        -- Cursor is in a file → jump to Neo-tree
-        vim.cmd("Neotree focus")
-      end
-    end,
-    desc = "Focus Neo-tree or close if already focused",
-    silent = true,
+        if ft == "neo-tree" then
+          -- Cursor is inside Neo-tree → close it
+          vim.cmd("Neotree close")
+        else
+          -- Cursor is in a file → jump to Neo-tree
+          vim.cmd("Neotree focus")
+        end
+      end,
+      desc = "Focus Neo-tree or close if already focused",
+      silent = true,
+    },
   },
-},
   opts = {
     filesystem = {
       window = {
