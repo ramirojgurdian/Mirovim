5d0ff4a9bb46e49c336777bdfc16931bab3e693b
Ramiro Gurdian <miro@Ramiros-MacBook.local>
2026-06-06 15:35:58 -0500

    adding html formatting and minor refactors

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,20 +1,20 @@
-require('lazy').setup({
+require("lazy").setup({
 
-  --If you are not zyo you may want to delete this one
-  --require('plugins.smear-cursor'),
-  -----------------------------------------------------
-  require('plugins.neo-tree'),
-  require('plugins.mason'),
-  require('plugins.which-key'),
-  require('plugins.telescope'),
-  require('plugins.lazydev'),
-  require('plugins.lspconfig'),
-  require('plugins.colorscheme'),
-  require('plugins.treesitter'),
-  require('plugins.conform'),
-  require('plugins.autopairs'),
-  require('plugins.gitsigns'),
-  require('plugins.lualine'),
-  require('plugins.render-markdown'),
-  require('plugins.roslyn')
+	--If you are not zyo you may want to delete this one
+	--require('plugins.smear-cursor'),
+	-----------------------------------------------------
+	require("plugins.neo-tree"),
+	require("plugins.mason"),
+	require("plugins.which-key"),
+	require("plugins.telescope"),
+	require("plugins.lazydev"),
+	require("plugins.lsp.lspconfig"),
+	require("plugins.colorscheme"),
+	require("plugins.treesitter"),
+	require("plugins.conform"),
+	require("plugins.autopairs"),
+	require("plugins.gitsigns"),
+	require("plugins.lualine"),
+	require("plugins.render-markdown"),
+	require("plugins.roslyn"),
 })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,27 @@
-return {  -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  config = function()
-    local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim',
-      'vimdoc', 'rust', 'c-sharp' }
-    require('nvim-treesitter').install(filetypes)
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = filetypes,
-      callback = function() vim.treesitter.start() end,
-    })
-  end,
+return { -- Highlight, edit, and navigate code
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local filetypes = {
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+			"rust",
+			"c_sharp",
+		}
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = filetypes,
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+	end,
 }
